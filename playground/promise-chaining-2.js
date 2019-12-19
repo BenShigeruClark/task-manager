832e09252d911bc2a6813665c7fd2b9d51e9f187
Ben <benclarkphoto@mac.com>
2019-12-18 23:13:24 -0800

    async await for task

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5df334b2e78b6d0bd6d71d09', { completed: true }).then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: true })
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('5df334b2e78b6d0bd6d71d09', { completed: true }).then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: true })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })  
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('5df33392892c280b687a3fe6').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
-})  
+})
