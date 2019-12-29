884fbfedf3acc740123a83ce5483b2c9d34fe5d5
Ben <benclarkphoto@mac.com>
2019-12-28 21:06:24 -0800

    Express middleware with GET and POST

--- src/index.js
+++ src/index.js
@@ -6,8 +6,22 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+app.use((req, res, next) => {
+    res.status(503).send('503 Server is not available.  Try again in a few minutes!')
+})
+
 app.use(express.json())
 app.use(userRouter, taskRouter)
+
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
