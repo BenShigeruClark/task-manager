8ebfa27b4059dc670c85423caec9abba12a9b3f8
Ben <benclarkphoto@mac.com>
2020-01-01 15:09:43 -0800

    Update task by id

--- src/routers/task.js
+++ src/routers/task.js
@@ -45,7 +45,8 @@ router.get('/tasks/:id', auth, async (req, res) => {
   }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+// updates a task by id
+router.patch('/tasks/:id', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['completed', 'description']
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -55,15 +56,14 @@ router.patch('/tasks/:id', async (req, res) => {
   }
 
   try {
-      const task = await Task.findById(req.params.id)
-
-      updates.forEach((update) => task[update] = req.body[update])
-      await task.save()
+      const task = await Task.findOne({ _id: req.params.id, owner: req.user._id }) 
 
       if (!task) {
           return res.status(404).send()
       }
 
+      updates.forEach((update) => task[update] = req.body[update])
+      await task.save()
       res.send(task)
   } catch (e) {
           res.status(400).send()
