36cfcf93c2c4db9f42d7c56b9ebe4e4bfebf10ae
Ben <benclarkphoto@mac.com>
2020-01-03 22:17:25 -0800

    Delete middlewear ex

--- src/index.js
+++ src/index.js
@@ -6,17 +6,10 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
 
-// app.use((req, res, next) => {
-//     res.status(503).send('503 Server is not available.  Try again in a few minutes!')
-// })
+const multer = require('multer')
+
+
 
 app.use(express.json())
 app.use(userRouter, taskRouter)
@@ -27,17 +20,3 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('5e0ae740954ff6d46efc4382')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    const user = await User.findById('5e0d3811d2966b0385024f00')
-    await user.populate('tasks').execPopulate() // finds all tasks and stores on user array
-    console.log(user.tasks)
-}
-
-main()
