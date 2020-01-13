cb20b505e4fcb1d26d123e1033dc525cb757dfee
Ben <benclarkphoto@mac.com>
2020-01-12 16:50:34 -0800

    create jest testing framework

---
+++ tests/math.test.js
@@ -0,0 +1,12 @@
+// Why should you write test cases
+// Saves time, creates reliable software, gives flexibility to developers for refactoring, collaboration, and profiling.
+// Gives dev peace of mind that application works!
+test('Hello World', () => {
+
+})
+
+test('This should fail', () => {
+    throw new Error('Failure!')
+})
+
+
