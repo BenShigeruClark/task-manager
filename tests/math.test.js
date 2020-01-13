a58254620bb82b37c33b4b33c08c792598bf4907
Ben <benclarkphoto@mac.com>
2020-01-12 17:13:10 -0800

    Create function and test cases

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,12 +1,17 @@
+const { calculateTip } = require('../src/math')
+
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+    expect(total).toBe(13)
+})
+
+test('Should calculate total with default tip', () => {
+    const total = calculateTip(10)
+    expect(total).toBe(12.5)
+})
+
 // Why should you write test cases
 // Saves time, creates reliable software, gives flexibility to developers for refactoring, collaboration, and profiling.
 // Gives dev peace of mind that application works!
-test('Hello World', () => {
-
-})
-
-test('This should fail', () => {
-    throw new Error('Failure!')
-})
 
 
